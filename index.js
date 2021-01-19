a4078dab8ca2dadd23c6020583ab1c73f066f7bb
Afzal-Bidhan <afzalbidhan@gmail.com>
2021-01-19 12:38:34 +0600

    update code

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
+
 // website
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
@@ -26,92 +27,109 @@ client.connect(err => {
   //projectInfoCollection start
 
   // add projecti info
-  app.post("/addProjectInfo", (req, res) => {
+  app.post("/addProject_AB091_v5", (req, res) => {
     const info = req.body;
     projectInfoCollection.insertOne(info)
       .then(result => {
-        res.redirect('http://localhost:3000/admin')
-      })
-  })
+        console.log(result)
+        res.send(result);
+      });
+  });
   // read project data
-  app.get('/projectInfo', (req, res) => {
+  app.get('/projectvisit_AB007_v1', (req, res) => {
     projectInfoCollection.find({})
       .toArray((err, document) => {
         res.send(document);
-      })
+      });
   })
   // delete single project data
-  app.delete('/deleteProjectInfo/:id', (req, res) => {
+  app.delete('/deleteProject_AB097_v2/:id', (req, res) => {
     projectInfoCollection.deleteOne({ _id: req.params.id })
       .then((result) => {
-        res.send(result.deletedCount > 0);
-      })
-  })
+        res.send(result);
+      });
+  });
   // single Project
-  app.get('/singleProject/:id', (req, res) => {
+  app.get('/singleProject_AB836_v3/:id', (req, res) => {
     projectInfoCollection.find({ _id: req.params.id })
       .toArray((err, document) => {
         res.send(document[0]);
       });
   });
   // update Project
-  app.patch('/updateProject/:id', (req, res) => {
+  app.patch('/updateProject_AB865_v4/:id', (req, res) => {
     projectInfoCollection.updateOne({ _id: req.params.id },
       {
-        $set: { projectImage: req.body.projectImage, projectName: req.body.projectName, projectType: req.body.projectType, projectCategory: req.body.projectCategory, projectDuration: req.body.projectDuration, technologies: req.body.technologies, livePreview: req.body.livePreview, sourceCode: req.body.sourceCode }
+        $set: {
+          name: req.body.name,
+          image: req.body.image,
+          image: req.body.image,
+          imageOne: req.body.imageOne,
+          imageTwo: req.body.imageTwo,
+          imageThree: req.body.imageThree,
+          type: req.body.type,
+          category: req.body.category,
+          duration: req.body.duration,
+          technologies: req.body.technologies,
+          livePreview: req.body.livePreview,
+          sourceCode: req.body.sourceCode,
+          description: req.body.description
+        }
       })
       .then(result => {
-        res.send(result.modifiedCount > 0);
+        res.send(result);
         console.log(result)
-      })
-  })
-//********************************************************************************* */
+      });
+  });
+
+
+  //********************************************************************************* */
 
   // sociaCollection start
 
   // read project data
-  app.get('/socialLink', (req, res) => {
+  app.get('/socialLinksVisit_AB065_v1', (req, res) => {
     sociaCollection.find({})
       .toArray((err, document) => {
         res.send(document);
-      })
-  })
+      });
+  });
   // single Social Link
-  app.get('/singleSocialLink/:id', (req, res) => {
+  app.get('/singleSocialLink_AB782_v3/:id', (req, res) => {
     sociaCollection.find({ _id: req.params.id })
       .toArray((err, document) => {
         res.send(document[0]);
       });
   });
   // add social Link
-  app.post("/addSocialLink", (req, res) => {
+  app.post("/addSocialLink_AB830_v5", (req, res) => {
     const product = req.body;
     sociaCollection.insertOne(product)
       .then(result => {
         console.log(result)
-        res.redirect('http://localhost:3000/admin')
+        res.send(result);
       })
-  })
+
+  });
   //update Social Link
-  app.patch('/updateSocialLink/:id', (req, res) => {
+  app.patch('/updateSocialLink_AB154_v4/:id', (req, res) => {
     sociaCollection.updateOne({ _id: req.params.id },
       {
         $set: { socialName: req.body.socialName, socialURL: req.body.socialURL }
       })
       .then(result => {
-        console.log(result)
-      })
-  })
+        console.log(result);
+        res.send(result);
+      });
+  });
   // delete Social Link
-  app.delete('/deleteSingleSocialLink/:id', (req, res) => {
+  app.delete('/deleteSocialLink_AB076_v2/:id', (req, res) => {
     sociaCollection.deleteOne({ _id: req.params.id })
       .then((result) => {
-        res.send(result.deletedCount > 0);
-      })
-  })
+        res.send(result);
+      });
+  });
 
 });
 
 app.listen(process.env.PORT || 9000);
-
-
